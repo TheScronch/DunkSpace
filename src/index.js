7d9b0686baeaf272caab54cfed3c4e1c33912a33
TheScronch <duncanyewell@gmail.com>
2018-05-25 01:17:55 -0400

    test

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App title="DunkSpace"/>, document.getElementById('root'));
+ReactDOM.render(<App title="DunkSpace!"/>, document.getElementById('root'));
 registerServiceWorker();
